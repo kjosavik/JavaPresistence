8ed14fa8a0bcf5d984d59f9152a6d0b6e4f99a73
Erik Kjosavik <Erik Kjosavik>
2017-03-03 21:51:04 +0100

    Siste commit før jeg legger meg, fredag

--- src/Konto.java
+++ src/Konto.java
@@ -2,4 +2,41 @@
  * Created by Kjosavik on 3/3/2017.
  */
 public class Konto {
+
+    private int kontonummer;
+    private double saldo;
+    private String eier;
+
+    public String getEier() {
+        return eier;
+    }
+
+    public void setEier(String eier) {
+        this.eier = eier;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getKontonummer() {
+
+        return kontonummer;
+    }
+
+    public void setKontonummer(int kontonummer) {
+        this.kontonummer = kontonummer;
+    }
+
+    public void trekk(double belop){
+        saldo -= belop;
+    }
+
+    public String toString (){
+        return "Kontonummer: " + kontonummer + ", Saldo: " + saldo + " Eier: " + eier;
+    }
 }
